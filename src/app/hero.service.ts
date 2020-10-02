9577652db9aaeaf3a9e7c6b23edc1186b30d6d35
yoorw <yoorw@hotmail.com>
2020-10-02 04:56:37 -0500

    Section 5: Add a default route

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,6 @@
+import { Injectable } from '@angular/core';
 import {Observable, of} from 'rxjs';
 
-import { Injectable } from '@angular/core';
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 import {MessageService} from './message.service';
