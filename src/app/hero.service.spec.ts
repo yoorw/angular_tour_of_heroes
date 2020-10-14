efd862e75b1671fb5109b29ae647765bb04016ed
yoorw <yoorw@hotmail.com>
2020-10-14 04:59:28 -0500

    added tests for hero.service.spec.ts only

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,16 +1,45 @@
-import { TestBed } from '@angular/core/testing';
-
 import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
 
 describe('HeroService', () => {
+  const mockHeroes: Hero[] = [
+    {id: 1, name: 'test1'},
+    {id: 2, name: 'test2'},
+    {id: 3, name: 'test3'}
+  ];
+  
   let service: HeroService;
+  // let messageService: MessageService;
+
+  let http = {
+    get: jest.fn(() => of(mockHeroes))
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HeroService);
-  });
+    const messageService = new MessageService();
+    messageService = jest.fn();
+    // messageService = jest.fn();
+    service = new HeroService(http as any, messageService);
+  })
+
+
+
+  // beforeEach(() => {
+  //   TestBed.configureTestingModule({});
+  //   service = TestBed.inject(HeroService);
+  // });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('getHeroes: should return a list of heroes', () => {
+    service.getHeroes().subscribe(heroes => {
+      expect(http.get).toHaveBeenCalledWith('api/heroes');
+      expect(heroes.length).toBe(3);
+      expect(heroes[2].name).toBe('test3');
+    });
   });
 });
